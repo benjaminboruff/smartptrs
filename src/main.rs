cf33439147998273cecf3c789e472999bd22f467
Benjamin H Boruff <11185240+benjaminboruff@users.noreply.github.com>
2022-12-18 01:54:20 -0500

    basic tracker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 // use std::ops::Deref;
 
 // struct MyBox<T>(T);
@@ -36,6 +37,9 @@ use std::rc::Rc;
 
 fn main() {
     // let x = 5;
+    // let y = &mut x;
+
+    // let x = 5;
     // let y = MyBox::new(x);
 
     // assert_eq!(5, x);
@@ -53,15 +57,15 @@ fn main() {
     // drop(c);
     // println!("CustomSmartPointers created.");
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // println!("count after creating a = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // println!("count after creating b = {}", Rc::strong_count(&a));
+    // {
+    //     let c = Cons(4, Rc::clone(&a));
+    //     println!("count after creating c = {}", Rc::strong_count(&a));
+    // }
+    // println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
 // fn hello(name: &str) {
